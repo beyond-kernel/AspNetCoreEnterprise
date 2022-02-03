40516ede699e3c4bcd2db34c3378dfd11027de05
Rafael <rafanog.linux@gmail.com>
2022-02-03 20:46:38 -0300

    Implementação de DelegateHandlers para roteamento e autorização e Polly para resiliência da aplicação

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,8 @@
 ﻿using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
+using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 
 namespace NSE.WebApp.MVC.Configuration
 {
@@ -7,9 +10,28 @@ namespace NSE.WebApp.MVC.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddTransient<HttpClientAuthorizationDelegatingHandler>();
+
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            services.AddHttpClient<ICatalogoService, CatalogoService>();
+            var htttpPolicyExtensions = HttpPolicyExtensions.HandleTransientHttpError().WaitAndRetryAsync(new[]
+            {
+                TimeSpan.FromSeconds(1),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(10),
+
+            }, (outcome, timespan, retryCount, context) =>
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"Tentando pela  {retryCount} vez");
+                Console.ForegroundColor = ConsoleColor.White;
+            });
+
+            services.AddHttpClient<ICatalogoService, CatalogoService>()
+                .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+                //.AddTransientHttpErrorPolicy(p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromSeconds(600)));
+                .AddPolicyHandler(htttpPolicyExtensions);
+
 
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
