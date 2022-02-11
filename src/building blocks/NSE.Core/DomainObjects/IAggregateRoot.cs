affc1c92f6ddd99d17fcba3105535cbc29e679f7
Rafael <rafanog.linux@gmail.com>
2022-02-10 22:04:38 -0300

    correcao comentario

--- src/building blocks/NSE.Core/DomainObjects/IAggregateRoot.cs
+++ src/building blocks/NSE.Core/DomainObjects/IAggregateRoot.cs
@@ -1,4 +1,4 @@
 ﻿namespace NSE.Core.DomainObjects
 {
-    public interface IAggregateRoot { } //interface de marcacao para agregacao de tabelas filhas sem obrigacao de implemntar todas as regrtas do contrato 
+    public interface IAggregateRoot { } //interface de marcacao para agregacao de tabelas filhas sem obrigacao de implementar todas as regrtas do contrato 
 }
