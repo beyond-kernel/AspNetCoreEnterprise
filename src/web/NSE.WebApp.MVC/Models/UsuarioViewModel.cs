472802fd1b80e53430a014bb03928a454a685990
Rafael <rafanog.linux@gmail.com>
2022-02-22 09:35:09 -0300

    adicionado campos nullables

--- src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/UsuarioViewModel.cs
@@ -18,12 +18,12 @@ namespace NSE.WebApp.MVC.Models
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [EmailAddress(ErrorMessage = "O campo {0} esta em formato inválido")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
-        public string Senha { get; set; }
+        public string? Senha { get; set; }
         [Compare("Senha", ErrorMessage = "As senhas não conferem")]
-        public string SenhaConfirmacao { get; set; }
+        public string? SenhaConfirmacao { get; set; }
     }
 
     public class UsuarioLogin
