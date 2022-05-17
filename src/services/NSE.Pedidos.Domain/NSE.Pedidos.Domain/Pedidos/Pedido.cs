f3312f5a1be274e32ae4fba04cad264d2e4c8d45
Rafael <rafanog.linux@gmail.com>
2022-05-17 20:32:49 -0300

    atualizacao orquestrador de pedidos e query de pedidos

--- src/services/NSE.Pedidos.Domain/NSE.Pedidos.Domain/Pedidos/Pedido.cs
+++ src/services/NSE.Pedidos.Domain/NSE.Pedidos.Domain/Pedidos/Pedido.cs
@@ -43,6 +43,15 @@ namespace NSE.Pedidos.Domain.Pedidos
         {
             PedidoStatus = PedidoStatus.Autorizado;
         }
+        public void CancelarPedido()
+        {
+            PedidoStatus = PedidoStatus.Cancelado;
+        }
+
+        public void FinalizarPedido()
+        {
+            PedidoStatus = PedidoStatus.Pago;
+        }
 
         public void AtribuirVoucher(Voucher voucher)
         {
